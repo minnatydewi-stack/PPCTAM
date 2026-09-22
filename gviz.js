e776a3ec56ff5d246298600ea8d0cc9aca2dc872
minnatydewi-stack <minnatydewi@gmail.com>
2026-09-22 15:46:10 +0700

    Update gviz.js

--- gviz.js
+++ gviz.js
@@ -7,9 +7,9 @@
  * -------------------------------------------------------------
  */
 
-const GViz = gviz/tq?tqx=out:json
-  function CONFIG.1FAbcl_l4uxs7KlGFuKRABs_C5HM-qvAiOcPmF-4DqZk 
-    const base = `https://docs.google.com/spreadsheets/d/1FAbcl_l4uxs7KlGFuKRABs_C5HM-qvAiOcPmF-4DqZk/edit?usp=sharing`;
+const GViz = (() => {
+  function buildUrl(sheetName) {
+    const base = `https://docs.google.com/spreadsheets/d/${CONFIG.SPREADSHEET_ID}/gviz/tq?tqx=out:json`;
     return sheetName ? `${base}&sheet=${encodeURIComponent(sheetName)}` : base;
   }
 
