14dbd0fe321759a21c63f61cb04d21333467c4e1
minnatydewi-stack <minnatydewi@gmail.com>
2026-09-22 15:38:48 +0700

    Fix CONFIG.SPREADSHEET_ID assignment

--- gviz.js
+++ gviz.js
@@ -8,7 +8,7 @@
  */
 
 const GViz = (() => {
-  function CONFIG.SPREADSHEET_ID dan gviz/tq?tqx=out:json
+  function CONFIG.1FAbcl_l4uxs7KlGFuKRABs_C5HM-qvAiOcPmF-4DqZk gviz/tq?tqx=out:json
     const base = `https://docs.google.com/spreadsheets/d/1FAbcl_l4uxs7KlGFuKRABs_C5HM-qvAiOcPmF-4DqZk/edit?usp=sharing`;
     return sheetName ? `${base}&sheet=${encodeURIComponent(sheetName)}` : base;
   }
